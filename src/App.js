019e7592bccf5d747ce49bdcd8ce918ef8f366cc
dilrong <wet932@naver.com>
2019-04-05 16:19:37 +0900

    create button function add kakaoFriend, link weatly.com

--- src/App.js
+++ src/App.js
@@ -3,13 +3,21 @@ import { Button } from './components';
 import './App.scss';
 
 class App extends Component {
+  componentDidMount(){
+    window.Kakao.init('88d4f7e748615e03809e9f5d4f067fc5');
+  }
+
+  addKakaoFriend = () => {
+    window.Kakao.PlusFriend.addFriend({plusFriendId: '_xmfxiTj'})
+  }
+
   render() {
     return (
       <div className="App">
         <img className="slogan" alt='slogan' src='/images/slogan.svg'/>
         <img className="phone" alt='phone' src='/images/phone.png'/> 
-        <Button text={["카카오톡 ", <span className="button__text">@위틀리</span>, " 추가하고 할인받기"]}></Button>
-        <Button text={[<span className="button__text">위틀리 홈페이지</span>, "에서 할인받기"]}></Button>
+        <Button text={["카카오톡 ", <span className="button__text">@위틀리</span>, " 추가하고 할인받기"]} onClick={this.addKakaoFriend}></Button>
+        <a href="https://weatly.com" target="_blank" rel="noopener noreferrer" ><Button text={[<span className="button__text">위틀리 홈페이지</span>, "에서 할인받기"]}></Button></a>
       </div>
     );
   }
--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import './Button.scss';
 
-const Button = ({text}) => (
-    <button className='button'>
+const Button = ({text, onClick}) => (
+    <button className='button' onClick={onClick}>
         {text}
     </button>
 );
